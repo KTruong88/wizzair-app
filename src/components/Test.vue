fb5b00398bd604fbd0b21a64596a14fa08a0fcfe
Kevin Truong <kevintru0ng@yahoo.com>
2018-03-22 13:06:56 -0700

    test

---
+++ src/components/Test.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="test">
+    <h1>{{ title }}</h1>
+    <p v-if="showName">{{ user.firstName }}</p>
+    <p v-else>Nobody</p>
+    <ul>
+      <li id="center" v-for="item in items">{{ item.title }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  data() {
+    return {
+      title: '<h1>Welcome to Wizzair</h1>',
+      user: {
+        firstName: 'John',
+        lastName: 'Doe'
+      },
+      showName: true,
+      items: [
+        { title: 'Item One' },
+        { title: 'Item Two' },
+        { title: 'Item Three' }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+  #center {
+    align-items: center;
+    
+  }
+</style>
